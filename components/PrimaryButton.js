2b126837cedddeefa80ccafca5922e1ed2f243fa
Rom943 <roma19943@gmail.com>
2024-02-18 15:00:58 +0200

    18.2.24

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,36 +1,43 @@
-import { View,Text, Pressable, StyleSheet } from "react-native"
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
-export default function PrimaryButton ({children}){
-    function pressHandler (){
-
-    }
-    return (
-        <View style={styles.buttonContainer}>
-        <Pressable onPress={pressHandler} >
-            <Text style={styles.buttonText}>{children}</Text>
-        </Pressable>
-        </View>
-    )
+export default function PrimaryButton({ children }) {
+  function pressHandler() {}
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.pressed, styles.buttonInnerContainer]
+            : styles.buttonInnerContainer
+        }
+        onPress={pressHandler}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    buttonContainer:{
-        backgroundColor:'#f11364',
-        borderRadius:28,
-        paddingVertical:8,
-        paddingHorizontal:16,
-        margin:4,
-    },
-    buttonText:{
-        color:'white',
-        textAlign:'center'
-    },
-    buttonOuterContainer:{
-        borderRadius:28,
-        margin:4,
-        overflow:"hidden",
-    },
-    buttonInnerContainer:{
+  buttonInnerContainer: {
+    backgroundColor: "#850561",
 
-    }
-})
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+    borderRadius: 28,
+    margin: 4,
+    minWidth: 120,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,40 +1,56 @@
-import { TextInput, View } from "react-native"
-import PrimaryButton from "../components/PrimaryButton"
-import { StyleSheet } from "react-native"
+import { TextInput, View } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+import { StyleSheet } from "react-native";
 
-
-export default function StartGameScreen (){
-return(
-<View style={styles.inputContainer}>
-    <TextInput style={styles.textInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false}/>
-    <PrimaryButton>Reset</PrimaryButton>
-    <PrimaryButton>Confirm</PrimaryButton>
-</View>
-) 
-
+export default function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          style={styles.textInput}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+      </View>
+      <View style={styles.buttonsArea}>
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    inputContainer:{
-        padding:16,
-        marginTop:100,
-        backgroundColor:'#72065c',
-        marginHorizontal: 24,
-        borderRadius:8,
-        shadowColor:'black',
-        shadowOffset:{width:3,height:3},
-        shadowRadius:6,
-        shadowOpacity:0.25
-    },
-    textInput:{
-        height:50,
-        fontSize:32,
-        borderBottomColor:"#ddb52f",
-        borderBottomWidth:2,
-        color:"#ddb52f",
-        marginVertical:8,
-        fontWeight:'bold',
-        width:50,
-        textAlign:'center'
-    }
-})
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#530343",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 3, height: 3 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  textInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    width: 50,
+    textAlign: "center",
+  },
+  textInputContainer: {
+    alignItems: "center",
+  },
+  buttonsArea: {
+    flexDirection: "row",
+    justifyContent:"center",
+    marginTop:10
+  },
+});
